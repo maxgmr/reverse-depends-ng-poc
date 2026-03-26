801f07c4d0960ae684b982ecbaa5098df2c7e861
Max Gilmour <max.gilmour@canonical.com>
2026-03-26 13:36:59 -0700

    Add tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use reverse_depends_ng_poc::Args;
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() {
+    let args = Args::parse();
+    if let Err(e) = run(args).await {
+        // anyhow's `{e:#}` format prints the full error chain.
+        eprintln!("error: {e:#}");
+        std::process::exit(1);
+    }
+}
+
+async fn run(args: Args) -> anyhow::Result<()> {
+    todo!()
 }
